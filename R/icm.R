303cfca3db46bccaceb16226c1d2f1b6bfda5429
SpatLyu <lyu.geosocial@gmail.com>
2024-12-21 19:28:46 +0800

    update `icm` docs

--- R/icm.R
+++ R/icm.R
@@ -13,8 +13,9 @@
 #' d = c(rep(1,7),rep(2,3),rep(1,3),rep(2,7))
 #' s = rep(1:2, each = 10)
 #' demodf = data.frame(d = d, s = s)
+#' \donttest{
 #' icm(d ~ s, data = demodf)
-#'
+#' }
 icm = \(formula, data, unit = c("e","2","10"),
         seed = 42, permutation_number = 999){
   if (inherits(data,"sf")){
